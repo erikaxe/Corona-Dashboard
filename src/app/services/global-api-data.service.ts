be69b0f631f8b65b18af93aef70cb9636aabe6c2
Erik <erikio93@hotmail.com>
2021-02-21 15:25:31 +0100

    Added error handler in service

--- src/app/services/global-api-data.service.ts
+++ src/app/services/global-api-data.service.ts
@@ -1,9 +1,9 @@
+// Imports
 import { Injectable } from '@angular/core';
-
-import { HttpClient, /* HttpErrorResponse */ } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-/* import { throwError as observableThrowError } from 'rxjs';
-import { catchError } from 'rxjs/operators'; */
+import { throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,14 @@ export class GlobalApiDataService {
 
   // Function to get yesterdays global data from API
   getGlobalData(): Observable<any> {
-    const url = 'https://corona.lmao.ninja/v2/all?yesterday';
-    return this.http.get<any>(url);
+    const url = 'https://corona.lmao.ninja/v2/all';
+    // Return global data, if error, errorHandler will trigger
+    return this.http.get<any>(url).pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    // Return error message from Angular OR return 'Server error'
+    return observableThrowError(error.message || 'Server error');
   }
 
 }
