5da04fc96df642796990eb921cf335c5d14a93eb
Erik <erikio93@hotmail.com>
2021-03-04 22:25:47 +0100

    Chart works as intended

--- src/app/components/user-interactive-chart/user-interactive-chart.component.ts
+++ src/app/components/user-interactive-chart/user-interactive-chart.component.ts
@@ -1,5 +1,5 @@
 // Imports
-import { Component, OnInit, ViewChild, ApplicationRef } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
 // Service import
 import { ApiDataService } from './../../services/api-data.service';
@@ -15,10 +15,6 @@ import {
   ApexTitleSubtitle,
   ApexYAxis,
 } from 'ng-apexcharts';
-
-
-// Import chart model
-/* import { Chart, Serie, Category } from '../../shared/chart'; */
 
 // Export chart options
 export type ChartOptions = {
@@ -38,21 +34,11 @@ export type ChartOptions = {
 })
 
 export class UserInteractiveChartComponent implements OnInit {
-  // Chart should not be static
-  @ViewChild('chart', {static: false}) chart!: ChartComponent;
+  @ViewChild('chart') chart!: ChartComponent;
   public chartOptions!: Partial<ChartOptions>;
-
-  // is chartDataLoaded ?
-  chartDataLoaded = false;
 
   // Array that contains all countries from the API
   countriesArray = [] as any;
-
-  // Array to contain all information from specified country, IS EMPTY ATM
-  /* countryInfo = [] as any; */
-
-  // Array to contain all picked countries, IS EMPTY ATM
-  /* chartCountries = [] as any; */
 
   // Array that contains the country the user selected
   country = [] as any;
@@ -61,23 +47,28 @@ export class UserInteractiveChartComponent implements OnInit {
   cases = [] as any;
   recovered = [] as any;
   deaths = [] as any;
-  categories = [] as any;
 
-  constructor(private apiDataService: ApiDataService, private cdr: ApplicationRef) {
+  // Force xaxis to be ApexXAxis
+  xaxis = {
+    categories: []
+  } as ApexXAxis;
+
+  constructor(private apiDataService: ApiDataService) {
+    // Chart starts
     this.chartOptions = {
-      // Chart bars for each category, place variables as data
+      // Chart bars for each category
       series: [
         {
           name: 'Confirmed',
-          data: this.cases
+          data: []
         },
         {
           name: 'Recovered',
-          data: this.recovered
+          data: []
         },
         {
           name: 'Deaths',
-          data: this.deaths
+          data: []
         }
       ],
       title: {
@@ -104,96 +95,14 @@ export class UserInteractiveChartComponent implements OnInit {
           colors: ['#304758']
         }
       },
-      xaxis: {
-        // Place selected country in chart
-        // tslint:disable-next-line: max-line-length
-        categories: this.categories
-
-      },
+      xaxis: this.xaxis,
       yaxis: {
         title: {
           text: 'Cases'
         }
       },
     };
-  }
-
-  updateChartOptions(): void {
-    /* !!!!!!!!! DATAN SKICKAS ALDRIG HIT FRÅN getData(), Tror det är för updateChartOptions inte "väntar" in getData() !!!!!!!!! */
-    /* !!!!!!!!! Loopen körs nog aldrig för country är empty !!!!!!!!! */
-
-
-    // Variables for the data
-
-    /* console.log(this.chartCountries, 'chartCountries log från updateChartOptions');
-    console.log(this.countryInfo, 'countryInfo log från updateChartOptions'); */
-
-
-    /* for (const stats of this.countryInfo){
-      countryTotalCases.push(stats.cases);
-      countryTotalDeaths.push(stats.deaths);
-      countryTotalRecovered.push(stats.recovered);
-      console.log(countryTotalCases, 'log från loop');
-    }
-
-    console.log(this.countryInfo, 'countryInfo log från updateChartOptions');
-    console.log(this.chartCountries, 'chartCountries log från updateChartOptions'); */
-
-    this.chartOptions = {
-      // Chart bars for each category, place variables as data
-      series: [
-        {
-          name: 'Confirmed',
-          data: this.cases
-        },
-        {
-          name: 'Recovered',
-          data: this.recovered
-        },
-        {
-          name: 'Deaths',
-          data: this.deaths
-        }
-      ],
-      title: {
-        text: 'Compare countries',
-        align: 'left'
-      },
-      chart: {
-        type: 'bar',
-        height: 350
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          dataLabels: {
-            position: 'top'
-          }
-        }
-      },
-      dataLabels: {
-        enabled: true,
-        offsetY: -20,
-        style: {
-          fontSize: '12px',
-          colors: ['#304758']
-        }
-      },
-      xaxis: {
-        // Place selected country in chart
-        // tslint:disable-next-line: max-line-length
-        categories: this.categories
-
-      },
-      yaxis: {
-        title: {
-          text: 'Cases'
-        }
-      },
-    };
-    console.log(this.chartOptions.xaxis?.categories, 'Log från categories');
-    // Data is loaded show chart
-    this.chartDataLoaded = true;
+    // Chart ends
   }
 
   ngOnInit(): void {
@@ -201,7 +110,6 @@ export class UserInteractiveChartComponent implements OnInit {
     this.apiDataService.getCountries().subscribe((data) => {
       // Place the subscribed data into countriesArray
       this.countriesArray = data;
-      this.updateChartOptions();
     });
   }
 
@@ -217,38 +125,69 @@ export class UserInteractiveChartComponent implements OnInit {
 
   // Function to get data of specified country
   getData(): void {
+    // Get the specified data from the API
     this.apiDataService.getRealtimeData(this.country).subscribe((data) => {
-      // Get the specified data from the API
 
       // Call setData and put in the data
       this.setData(data);
-
-      console.log(data);
     });
   }
 
-  // Place data in variables
+  // Update chart
   setData(country: any): void {
-    const series = this.chartOptions.series;
-    // If series have 3 objects push data to variables
-    if (series?.length === 3){
-      this.cases.push(country.cases);
-      this.recovered.push(country.recovered);
-      this.deaths.push(country.deaths);
-      this.categories.push(country.country);
-    }
-    // Force render
-    this.cdr.tick();
+    // Push new data
+    this.cases.push(country.cases);
+    this.recovered.push(country.recovered);
+    this.deaths.push(country.deaths);
+    // Update categories to country names
+    this.xaxis.categories.push(country.country);
+    // Update chart series with new data
+    this.chartOptions.series = [
+      {
+        name: 'Confirmed',
+        data: this.cases
+      },
+      {
+        name: 'Recovered',
+        data: this.recovered
+      },
+      {
+        name: 'Deaths',
+        data: this.deaths
+      }
+    ];
+    //
+    this.chartOptions.xaxis = this.xaxis;
   }
 
   // Function to reset the chart
   resetChart(): void {
+    // Empty the arrays
     this.cases = [];
     this.recovered = [];
     this.deaths = [];
-    this.categories = [];
 
-    console.log('reset Chart button pressed');
+    // "Reset" to default ApexXAxis
+    this.xaxis = {
+      categories: []
+    } as ApexXAxis;
+
+    // Empty series
+    this.chartOptions.series = [
+      {
+        name: 'Confirmed',
+        data: this.cases
+      },
+      {
+        name: 'Recovered',
+        data: this.recovered
+      },
+      {
+        name: 'Deaths',
+        data: this.deaths
+      }
+    ];
+    this.chartOptions.xaxis = this.xaxis;
   }
 
   // Function to trigger disabled property
